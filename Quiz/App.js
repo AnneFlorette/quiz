419f390e56561c8bfad479d1ab375dcbc8193e18
Cyrille Carpentier <cyrille.carpentier@imie.fr>
2019-11-04 15:08:42 +0100

    debut appli + logo

--- Quiz/App.js
+++ Quiz/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Merci :D
+//       </Text>
+//     </View>
+//   );
+// }
+
+export default class Logo extends Component {
+  render() {
+    // let pic = {
+    //   uri: './assets/quiz.png'
+    // };
+    return (
+      //<Image source={pic} style={{ width: 193, height: 110 }} />
+      <Image source={require('./assets/quiz.png')} style={{ width: 300, height: 110 }} />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
