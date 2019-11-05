0083d1169dbb29fcd696b4afc58f117008416746
Cyrille Carpentier <cyrille.carpentier@imie.fr>
2019-11-05 09:55:02 +0100

    commit du lundi soir

--- Quiz/App.js
+++ Quiz/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 // export default function App() {
 //   return (
@@ -17,7 +17,36 @@ export default class Logo extends Component {
     // };
     return (
       //<Image source={pic} style={{ width: 193, height: 110 }} />
-      <Image source={require('./assets/quiz.png')} style={{ width: 300, height: 110 }} />
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Image
+            source={require("./assets/quiz.png")}
+            style={{
+              marginTop: 5,
+              width: 150,
+              height: 50,
+              resizeMode: "stretch"
+            }}
+          />
+        </View>
+        <View style={styles.content}>
+          <View style={styles.questionsWrapper}>
+            <Text>Lorem ipsum</Text>
+            <View style={styles.questions}></View>
+          </View>
+          <View style={styles.answersWrapper}>
+            <Text>Lorem ipsum</Text>
+            <View style={styles.answers}>
+              <Button title="answer1" onPress={Alert.alert("oui1")} />
+              {/* <Button title="answer2" onPress={Alert.alert("oui2")} /> */}
+            </View>
+            <View style={styles.answers}>
+              <Button title="answer3" onPress={Alert.alert("oui3")} />
+              {/* <Button title="answer4" onPress={Alert.alert("oui4")} /> */}
+            </View>
+          </View>
+        </View>
+      </View>
     );
   }
 }
@@ -25,8 +54,30 @@ export default class Logo extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 15,
+    justifyContent: "center",
+    alignItems: "center"
   },
+  header: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderBottomColor: "#e5e5d8",
+    borderBottomWidth: 1,
+    width: "100%"
+  },
+  content: {
+    flex: 8,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  questionsWrapper: {
+    flex: 2
+  },
+  questions: {},
+  answersWrapper: {
+    flex: 1,
+    flexDirection: "row"
+  },
+  answers: {}
 });
